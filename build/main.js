37e42ce671f47711c71b14bc130a3663b281c928
Darren Klein <darren.a.klein@gmail.com>
2017-09-24 21:07:02 -0400

    Move initial map center

--- build/main.js
+++ build/main.js
@@ -1,6 +1,7 @@
+// THIS IS OUR CORE JS FILE, WHICH IS BUNDLED TO public/scripts/bundle.js
 require("leaflet")
 const alignment = require("./alignment.js"),
-	map = L.map("map", {"zoomControl": false}).setView([41.385334, -74.452829], 9)
+	map = L.map("map", {"zoomControl": false}).setView([41.384660, -74.473034], 12)
 
 L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png", {"maxZoom": 21}).addTo(map)
 L.control.scale({"position": "bottomright"}).addTo(map)
