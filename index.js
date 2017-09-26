9da7a99b2a773dfcdad635607693a9e467b14ebd
Darren Klein <darren.a.klein@gmail.com>
2017-09-25 21:52:05 -0400

    No need for ejs/views

--- index.js
+++ index.js
@@ -5,13 +5,8 @@ const express = require("express"),
 
 app.use(compression())
 app.use(express.static("public"))
-app.set("view engine", "ejs")
 app.use("/stylesheets", express.static(__dirname + "/node_modules/bootstrap/dist/css")) // eslint-disable-line prefer-template
 app.use("/stylesheets", express.static(__dirname + "/node_modules/leaflet/dist/")) // eslint-disable-line prefer-template
-
-app.get("/", (req, res) => {
-	res.render("index", {})
-})
 
 app.listen(port, () => {
 	console.log(`ExpressJS started on port ${port}`) // eslint-disable-line no-console
