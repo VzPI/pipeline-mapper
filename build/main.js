bbf047461bb2a05f0fc67d92970cc9e96130e8f5
Darren Klein <darren.a.klein@gmail.com>
2017-09-25 22:22:24 -0400

    Able to watchPosition and recenter map on user position

--- build/main.js
+++ build/main.js
@@ -1,8 +1,28 @@
 // THIS IS OUR CORE JS FILE, WHICH IS BUNDLED TO public/scripts/bundle.js
 require("leaflet")
 const mapData = require("./map_data.js"),
-	map = L.map("map", {"zoomControl": false}).setView([41.384660, -74.473034], 12)
+	map = L.map("map", {"zoomControl": false}).setView([41.384660, -74.473034], 12),
+	watchPositionButton = document.getElementById("watch-position"),
+	success = (position) => {
+		const latitude = position.coords.latitude,
+			longitude = position.coords.longitude,
+			accuracy = position.coords.accuracy
+
+		map.setView(new L.LatLng(latitude, longitude))
+		map.setZoom(19)
+	},
+	error = (err) => {
+		alert(`Error ${err.code}: ${err.message}`)
+	}
 
 L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png", {"maxZoom": 21}).addTo(map)
 L.control.scale({"position": "bottomright"}).addTo(map)
 mapData.addTo(map)
+
+watchPositionButton.addEventListener("click", () => {
+	navigator.geolocation.watchPosition(success, error, {
+		"maximumAge": 0, // DO NOT USE A CACHED POSITION, RETRIEVE CURRENT REAL POSITION
+		"timeout": Infinity, // DO NOT RETURN UNTIL THE POSITION IS AVAILABLE
+		"enableHighAccuracy": true
+	})
+})
