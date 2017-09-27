f6d52af7091343df4c20b6fd30ec48662efe7ecc
Darren Klein <darren.a.klein@gmail.com>
2017-09-26 21:04:20 -0400

    Added conditional to reset position of marker and radius

--- build/main.js
+++ build/main.js
@@ -1,18 +1,30 @@
 // THIS IS OUR CORE JS FILE, WHICH IS BUNDLED TO public/scripts/bundle.js
 require("leaflet")
 require("leaflet-pulse-icon")
+let marker,
+	radius
 const mapData = require("./map_data.js"),
 	map = L.map("map", {"zoomControl": false}).setView([41.384660, -74.473034], 12),
+	icon = L.icon.pulse(),
 	watchPositionButton = document.getElementById("watch-position"),
+	updateMarkerAndRadius = (latitude, longitude, accuracy) => {
+		if (marker && radius) {
+			marker.setLatLng([latitude, longitude])
+			radius.setLatLng([latitude, longitude], accuracy)
+		} else {
+			marker = L.marker([latitude, longitude], {"icon": icon}).addTo(map)
+			radius = L.circle([latitude, longitude], accuracy).addTo(map)
+		}
+
+		return false
+	},
 	success = (position) => {
 		const latitude = position.coords.latitude,
 			longitude = position.coords.longitude,
 			accuracy = position.coords.accuracy
 
 		map.setView(new L.LatLng(latitude, longitude))
-		map.setZoom(19)
-		L.marker([latitude, longitude], {"icon": L.icon.pulse()}).addTo(map)
-		L.circle([latitude, longitude], accuracy).addTo(map)
+		updateMarkerAndRadius(latitude, longitude, accuracy)
 	},
 	error = (err) => {
 		alert(`Error ${err.code}: ${err.message}`)
@@ -28,4 +40,5 @@ watchPositionButton.addEventListener("click", () => {
 		"timeout": Infinity, // DO NOT RETURN UNTIL THE POSITION IS AVAILABLE
 		"enableHighAccuracy": true
 	})
+	map.setZoom(19)
 })
