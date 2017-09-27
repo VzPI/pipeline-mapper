7328fda33ed9bcced47856cf01a379d8a6f2691a
Darren Klein <darren.a.klein@gmail.com>
2017-09-27 09:27:35 -0400

    Able to clearWatch, but there are some problems.

--- build/main.js
+++ build/main.js
@@ -2,11 +2,27 @@
 require("leaflet")
 require("leaflet-pulse-icon")
 let marker,
-	radius
+	radius,
+	isWatching = false // USED TO DETERMINE WHETHER OR NOT GEOLOCATION IS CURRENTLY RUNNING
 const mapData = require("./map_data.js"),
 	map = L.map("map", {"zoomControl": false}).setView([41.384660, -74.473034], 12),
 	icon = L.icon.pulse(),
 	watchPositionButton = document.getElementById("watch-position"),
+	watch = () => {
+		return navigator.geolocation.watchPosition(success, error, {
+			"maximumAge": 0, // DO NOT USE A CACHED POSITION, RETRIEVE CURRENT REAL POSITION
+			"timeout": 15000, // TRY TO ACCESS LOCATION FOR 15 SECONDS BEFORE THROWING TIMEOUT ERROR
+			"enableHighAccuracy": true
+		})
+	},
+	clearWatch = (watch) => {
+		isWatching = false
+		map.removeLayer(marker)
+		map.removeLayer(radius)
+		window.navigator.geolocation.clearWatch(watch)
+		// $("#watch_position").val("off");
+		// $("#watch_position").html("Watch position");
+	},
 	updateMarkerAndRadius = (latitude, longitude, accuracy) => {
 		if (marker && radius) {
 			marker.setLatLng([latitude, longitude])
@@ -25,10 +41,19 @@ const mapData = require("./map_data.js"),
 			accuracy = position.coords.accuracy
 
 		map.setView(new L.LatLng(latitude, longitude))
-		updateMarkerAndRadius(latitude, longitude, accuracy)
+		return updateMarkerAndRadius(latitude, longitude, accuracy)
 	},
 	error = (err) => {
-		alert(`Error ${err.code}: ${err.message}`)
+		let message
+
+		if (err.code === 3) {
+			message = "Could not get your location - please try again."
+		} else {
+			message = err.message
+		}
+
+		alert(`Error ${err.code}: ${message}`)
+		return clearWatch(watch) // NOT LIKELY TO BE NECESSARY, BUT CLEANUP JUST IN CASE
 	}
 
 L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png", {"maxZoom": 21}).addTo(map)
@@ -36,10 +61,11 @@ L.control.scale({"position": "bottomright"}).addTo(map)
 mapData.addTo(map)
 
 watchPositionButton.addEventListener("click", () => {
-	navigator.geolocation.watchPosition(success, error, {
-		"maximumAge": 0, // DO NOT USE A CACHED POSITION, RETRIEVE CURRENT REAL POSITION
-		"timeout": 50000, // WHY ISN'T Infinity WORKING? ON MOBILE, IT KEEPS ALERTING.
-		"enableHighAccuracy": true
-	})
-	map.setZoom(19)
+	if (isWatching) {
+		clearWatch()
+	} else {
+		isWatching = true
+		watch()
+		map.setZoom(19)
+	}
 })
