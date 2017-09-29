3b44d67421aabcf5c01174b3ea509cfde96274f4
Darren Klein <darren.a.klein@gmail.com>
2017-09-28 21:34:17 -0400

    Add heroku-ssl-redirect

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require("express"),
 	app = express(),
+	sslRedirect = require("heroku-ssl-redirect"),
 	compression = require("compression"),
-	port = process.env.PORT || 3000
+	port = process.env.PORT || 3000,
+	NODE_ENV = process.env.NODE_ENV || "development"
 
 app.use(compression())
 app.use(express.static("public"))
 app.use("/stylesheets", express.static(__dirname + "/node_modules/bootstrap/dist/css")) // eslint-disable-line prefer-template
 app.use("/stylesheets", express.static(__dirname + "/node_modules/leaflet/dist/")) // eslint-disable-line prefer-template
 app.use("/stylesheets", express.static(__dirname + "/node_modules/leaflet-pulse-icon/dist/")) // eslint-disable-line prefer-template
+
+if (NODE_ENV === "production") {
+	app.use(sslRedirect())
+}
 
 app.listen(port, () => {
 	console.log(`ExpressJS started on port ${port}`) // eslint-disable-line no-console
